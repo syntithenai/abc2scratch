2bd969f376b856135bf6ef612403af98eb413df3
root <root@code.2pisoftware.com>
2015-12-10 22:55:29 +1100

    add tests

---
+++ tests/unit/MyMusicTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class MyMusicTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+	$this->assertTrue(true);
+    }
+}
